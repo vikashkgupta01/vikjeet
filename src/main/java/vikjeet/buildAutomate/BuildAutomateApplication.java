176b9f620c1ca134faf9d9afba1b8fe39fff1c32
vikashkgupta01 <vikashkgupta01@gmail.com>
2024-11-23 17:23:01 +0530

    First commit to test changes

---
+++ src/main/java/vikjeet/buildAutomate/BuildAutomateApplication.java
@@ -0,0 +1,14 @@
+package vikjeet.buildAutomate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildAutomateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildAutomateApplication.class, args);
+		System.out.println("Application started!!!");
+	}
+
+}
---
+++ src/test/java/vikjeet/buildAutomate/BuildAutomateApplicationTests.java
@@ -0,0 +1,13 @@
+package vikjeet.buildAutomate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildAutomateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
