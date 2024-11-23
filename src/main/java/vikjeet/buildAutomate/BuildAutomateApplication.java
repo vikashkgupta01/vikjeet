57b83d5e1eb33006db7eebb3893eaa9fb111871a
vikashkgupta01 <vikashkgupta01@gmail.com>
2024-11-23 22:28:22 +0530

    modiying code for end program

--- src/main/java/vikjeet/buildAutomate/BuildAutomateApplication.java
+++ src/main/java/vikjeet/buildAutomate/BuildAutomateApplication.java
@@ -1,14 +1,29 @@
 package vikjeet.buildAutomate;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class BuildAutomateApplication {
 
+	@Value("${server.port}")
+	private int port;
 	public static void main(String[] args) {
-		SpringApplication.run(BuildAutomateApplication.class, args);
+		/*SpringApplication.run(BuildAutomateApplication.class, args);
+
 		System.out.println("Application started!!!");
+		System.out.println("port ::"+port);*/
+
+		ApplicationContext context = SpringApplication.run(BuildAutomateApplication.class, args);
+
+		// Access the Spring-managed bean
+		BuildAutomateApplication app = context.getBean(BuildAutomateApplication.class);
+		System.out.println("Application started!!!");
+		System.out.println("Port :: " + app.port);
+		System.exit(0);
+
 	}
 
 }
